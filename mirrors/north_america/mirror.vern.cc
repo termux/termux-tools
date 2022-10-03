7bf34eb86ea69ed2ccd85172d64a941e16065076
Henrik Grimler <henrik@grimler.se>
2022-10-03 19:28:38 +0200

    mirrors: add mirror.vern.cc, located in north america
    Ref: https://github.com/termux/termux-packages/issues/12252

---
+++ mirrors/north_america/mirror.vern.cc
@@ -0,0 +1,6 @@
+# This file is sourced by pkg
+# Mirror by vern.cc
+WEIGHT=1
+MAIN="https://mirror.vern.cc/termux/termux-main"
+ROOT="https://mirror.vern.cc/termux/termux-root"
+X11="https://mirror.vern.cc/termux/termux-x11"
