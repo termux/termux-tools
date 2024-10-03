fceac3e7b496ee44924a7f061c064ed6c48fe9fa
Saswata Sarkar <saswatasarkar13@gmail.com>
2024-10-03 07:27:15 +0530

    mirrors: replace mirrors.saswata.xyz with mirrors.saswata.cc (#126)
    Close #130 .

---
+++ mirrors/asia/mirrors.saswata.cc
@@ -0,0 +1,6 @@
+# This file is sourced by pkg
+# Mirror by saswatasarkar13, hosted in India
+WEIGHT=1
+MAIN="https://mirrors.saswata.cc/termux/termux-main"
+ROOT="https://mirrors.saswata.cc/termux/termux-root"
+X11="https://mirrors.saswata.cc/termux/termux-x11"
