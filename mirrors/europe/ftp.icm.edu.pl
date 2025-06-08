3cfaeb56778f89aeee39fbd6974a25dd0e160047
Kevin Williams <admin@utermux.dev>
2025-06-08 10:23:05 +0800

    mirrors: add ftp.icm.edu.pl
    Signed-off-by: Kevin Williams <admin@utermux.dev>

---
+++ mirrors/europe/ftp.icm.edu.pl
@@ -0,0 +1,6 @@
+# This file is sourced by pkg
+# Mirror by Warsaw University. Hosted in Warsaw, Poland.
+WEIGHT=1
+MAIN="https://ftp.icm.edu.pl/pub/Linux/dist/termux/termux-main"
+ROOT="https://ftp.icm.edu.pl/pub/Linux/dist/termux/termux-root"
+X11="https://ftp.icm.edu.pl/pub/Linux/dist/termux/termux-x11"
