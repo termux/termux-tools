66fc11dc954430e7f6f6e23c3e5b8e080464ea48
Ted Stein <me@tedstein.net>
2025-06-08 16:11:44 -0700

    mirrors: update contact info for icm.edu.pl and hacktegic.com
    md.mirrors.hacktegic.com: https://github.com/termux/termux-tools/issues/183
    ftp.icm.edu.pl: https://github.com/termux/termux-tools/issues/180

--- mirrors/europe/ftp.icm.edu.pl
+++ mirrors/europe/ftp.icm.edu.pl
@@ -1,5 +1,5 @@
 # This file is sourced by pkg
-# Mirror by Warsaw University. Hosted in Warsaw, Poland.
+# Mirror by Warsaw University / rzm1. Hosted in Warsaw, Poland.
 WEIGHT=1
 MAIN="https://ftp.icm.edu.pl/pub/Linux/dist/termux/termux-main"
 ROOT="https://ftp.icm.edu.pl/pub/Linux/dist/termux/termux-root"
--- mirrors/europe/md.mirrors.hacktegic.com
+++ mirrors/europe/md.mirrors.hacktegic.com
@@ -1,5 +1,5 @@
 # This file is sourced by pkg
-# Mirror by amocrenco. Hosted in Chisinau, Moldova.
+# Mirror by artiommocrenco. Hosted in Chisinau, Moldova.
 WEIGHT=1
 MAIN="https://md.mirrors.hacktegic.com/termux/termux-main"
 ROOT="https://md.mirrors.hacktegic.com/termux/termux-root"
