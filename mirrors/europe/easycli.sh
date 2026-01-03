4325b780ec37d3542eb6b9086af7efd0e91a532e
Sylirre <sylirre@termux.dev>
2026-01-03 21:46:31 +0000

    mirrors: add easycli.sh
    Mirror hosted by @sylirre, side-by-side with proot-distro rootfs repo.

---
+++ mirrors/europe/easycli.sh
@@ -0,0 +1,6 @@
+# This file is sourced by pkg
+# Mirror by Sylirre. Hosted in Germany, backed by Cloudflare.
+WEIGHT=1
+MAIN="https://easycli.sh/termux/termux-main"
+ROOT="https://easycli.sh/termux/termux-root"
+X11="https://easycli.sh/termux/termux-x11"
